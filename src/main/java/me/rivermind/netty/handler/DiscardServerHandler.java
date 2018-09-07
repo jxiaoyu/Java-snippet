04f2eab3d2b2bcbed4549a3af76af796e17f87d2
river <1990jiangxiaoyu@163.com>
2018-09-07 22:41:39 +0800

    add netty example

---
+++ src/main/java/me/rivermind/netty/handler/DiscardServerHandler.java
@@ -0,0 +1,26 @@
+package me.rivermind.netty.handler;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ *
+ * @author river
+ * @date 2016/5/30
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        // Discard the received data silently.
+        ((ByteBuf) msg).release();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
